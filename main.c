23abee6e3db22f63454cd5f045802ae9a618d18b
Zhunya-bz <fluffylion2013@gmail.com>
2021-06-12 22:39:10 +0300

    modufying code

--- main.c
+++ main.c
@@ -261,7 +261,7 @@ void from_A_to_B(p_list **head_a, p_list **head_b, t_var *par)
 	}
 }
 
-void from_B_to_A(p_list **head_a, p_list **head_b, t_var *par)
+void from_B_to_A(p_list **head_a, p_list **head_b, t_var *par, int c)
 {
 	int i;
 	int count;
@@ -272,28 +272,24 @@ void from_B_to_A(p_list **head_a, p_list **head_b, t_var *par)
 	count = 0;
 	while (tmp != NULL)
 	{
-		if (tmp->order > par->mid)
+		if (tmp->order >= par->mid)
 			count++;
 		tmp = tmp->next;
 	}
-//	if (par->size_b % 2 == 0)
-//		count = par->size_b / 2;
-//	else
-//		count = par->size_b / 2 + 1;
 	while (i < count)
 	{
-		if ((*head_b)->order == par->next)
+		if ((*head_b)->order >= par->mid)
+		{
+			ft_pa(head_a, head_b);
+			(*head_a)->flag = c;
+			i++;
+		}
+		else if ((*head_b)->order == par->next)
 		{
 			ft_pa(head_a, head_b);
 			ft_ra(head_a);
 			par->next++;
-			i++;
-		}
-		else if ((*head_b)->order > par->mid)
-		{
-			ft_pa(head_a, head_b);
-			(*head_a)->flag++;
-			i++;
+			//i++;
 		}
 		else
 			ft_rb(head_b);
@@ -387,7 +383,24 @@ int check_argument(char **argv, t_var *par, p_list **list_a, int *a)
 	return (1);
 }
 
+int check_top_bottom(p_list **lst, t_var *par)
+{
+	int count;
+	int i;
+	p_list *tmp;
 
+	count = 0;
+	i = 0;
+	tmp = (*lst);
+	while (tmp != NULL)
+	{
+		if (tmp->order == par->next)
+			count = i;
+		i++;
+		tmp = tmp->next;
+	}
+	return (count);
+}
 
 int main(int argc, char **argv)
 {
@@ -429,18 +442,257 @@ int main(int argc, char **argv)
 		from_A_to_B(&list_a, &list_b, &par);
 		par.flag++;
 		//print_list(&list_a, &list_b);
+		par.size_b = ft_lstsize_p(list_b);
+		int c;
+		c = 1;
+		while (par.size_b > 3)
+		{
+			par.max = ft_find_order(&list_b, &par);
+			//par.max = ft_lstsize_p(list_b);
+			par.mid = (par.max - par.next) / 2 + par.next;
+			from_B_to_A(&list_a, &list_b, &par, c);
+			par.size_b = ft_lstsize_p(list_b);
+			//par.flag++;
+			//print_list(&list_a, &list_b);
+			c++;
+		}
+		par.flag = c;
+		//print_list(&list_a, &list_b);
+//		if (par.size_b == 4)
+//		{
+//
+//		}
+		if (par.size_b == 3)
+		{
+			while (list_b->order != par.next)
+				ft_rb(&list_b);
+			if (list_b->order == par.next)
+			{
+				ft_pa(&list_a, &list_b);
+				ft_ra(&list_a);
+				par.next++;
+				par.size_b--;
+			}
+		}
+		if (par.size_b == 2)
+		{
+			if (list_b->order == par.next)
+			{
+				ft_pa(&list_a, &list_b);
+				ft_ra(&list_a);
+				par.next++;
+				par.size_b--;
+			}
+			else if (list_b->next->order == par.next)
+			{
+				ft_sb(&list_b);
+				ft_pa(&list_a, &list_b);
+				ft_ra(&list_a);
+				par.next++;
+				par.size_b--;
+			}
+			else
+			{
+				ft_pa(&list_a, &list_b);
+				//list_a->flag++;
+				ft_pa(&list_a, &list_b);
+				//list_a->flag++;
+				par.size_b -= 2;
+			}
+		}
+		if (par.size_b == 1)
+		{
+			if (list_b->order == par.next)
+			{
+				ft_pa(&list_a, &list_b);
+				ft_ra(&list_a);
+				par.next++;
+			}
+			else
+			{
+				ft_pa(&list_a, &list_b);
+				//list_a->flag++;
+			}
+		}
+		if (list_a->next->order == par.next)
+		{
+			ft_sa(&list_a);
+			ft_ra(&list_a);
+			par.next++;
+		}
+		while (list_a->order == par.next)
+		{
+			ft_ra(&list_a);
+			par.next++;
+		}
+		if (if_sort_list(&list_a))
+			return (0);
+		par.c = 0;
+		while (par.flag >= 0)
+		{
+			while ((list_a->flag == par.flag) && list_a->order != 1)
+			{
+				ft_pb(&list_a, &list_b);
+				par.c = 1;
+			}
+			if (par.c == 1)
+				break;
+			par.flag--;
+		}
+		//print_list(&list_a, &list_b);
+		int k = 0;
+		int i = 0;
 		while (!(if_sort_list(&list_a) && list_b == NULL))
 		{
 			//print_list(&list_a, &list_b);
 			par.size_b = ft_lstsize_p(list_b);
+//			while (par.size_b > 3)
+//			{
+//				par.max = ft_find_order(&list_b, &par);
+//				par.mid = (par.max - par.next) / 2 + par.next;
+//				from_B_to_A(&list_a, &list_b, &par);
+//				par.size_b = ft_lstsize_p(list_b);
+//				par.flag++;
+//			}
+			c = 1;
+//			while (list_b != NULL)
+//			{
+				if (par.flag == 0)
+				{
+					//par.flag++;
+					//print_list(&list_a, &list_b);
+					c = 1;
+					par.size_b = ft_lstsize_p(list_b);
+					while (par.size_b > 3 && list_b != NULL)
+					{
+						par.max = ft_find_order(&list_b, &par);
+						par.mid = (par.max - par.next) / 2 + par.next;
+						from_B_to_A(&list_a, &list_b, &par, c);
+						par.size_b = ft_lstsize_p(list_b);
+						//par.flag++;
+						c++;
+					}
+					par.flag = c;
+					if (par.size_b == 3)
+					{
+						while (list_b->order != par.next)
+							ft_rb(&list_b);
+						if (list_b->order == par.next)
+						{
+							ft_pa(&list_a, &list_b);
+							ft_ra(&list_a);
+							par.next++;
+							par.size_b--;
+						}
+					}
+					if (par.size_b == 2)
+					{
+						if (list_b->order == par.next)
+						{
+							ft_pa(&list_a, &list_b);
+							ft_ra(&list_a);
+							par.next++;
+							par.size_b--;
+						} else if (list_b->next->order == par.next)
+						{
+							ft_sb(&list_b);
+							ft_pa(&list_a, &list_b);
+							ft_ra(&list_a);
+							par.next++;
+							par.size_b--;
+						} else
+						{
+							ft_pa(&list_a, &list_b);
+							//list_a->flag++;
+							ft_pa(&list_a, &list_b);
+							//list_a->flag++;
+							par.size_b -= 2;
+						}
+					}
+					if (par.size_b == 1)
+					{
+						if (list_b->order == par.next)
+						{
+							ft_pa(&list_a, &list_b);
+							ft_ra(&list_a);
+							par.next++;
+						} else
+						{
+							ft_pa(&list_a, &list_b);
+							//list_a->flag++;
+						}
+					}
+					if (list_a->next->order == par.next)
+					{
+						ft_sa(&list_a);
+						ft_ra(&list_a);
+						par.next++;
+					}
+					while (list_a->order == par.next)
+					{
+						ft_ra(&list_a);
+						par.next++;
+					}
+					if (if_sort_list(&list_a))
+						return (0);
+					par.c = 0;
+					while (par.flag >= 0)
+					{
+						while ((list_a->flag == par.flag) && list_a->order != 1)
+						{
+							ft_pb(&list_a, &list_b);
+							par.c = 1;
+						}
+						if (par.c == 1)
+							break;
+						par.flag--;
+					}
+				}
+//				else
+//					break;
+//			}
+			par.size_b = ft_lstsize_p(list_b);
 			while (par.size_b > 3)
 			{
-				par.max = ft_find_order(&list_b, &par);
-				par.mid = (par.max - par.next) / 2 + par.next;
-				from_B_to_A(&list_a, &list_b, &par);
-				par.size_b = ft_lstsize_p(list_b);
-				par.flag++;
+				i = 0;
+				k = check_top_bottom(&list_b, &par);
+				if (k <= par.size_b / 2)
+				{
+					while (i < k)
+					{
+						ft_rb(&list_b);
+						i++;
+					}
+				}
+				else
+				{
+					while (i < par.size_b - k)
+					{
+						ft_rrb(&list_b);
+						i++;
+					}
+				}
+				if (list_b->order == par.next)
+				{
+					ft_pa(&list_a, &list_b);
+					ft_ra(&list_a);
+					par.next++;
+					par.size_b--;
+				}
 			}
+
+			if (list_a->next->order == par.next)
+			{
+				ft_sa(&list_a);
+				ft_ra(&list_a);
+				par.next++;
+			}
+			while (list_a->order == par.next)
+			{
+				ft_ra(&list_a);
+				par.next++;
+			}
+
 			if (par.size_b == 3)
 			{
 				while (list_b->order != par.next)
@@ -473,9 +725,9 @@ int main(int argc, char **argv)
 				else
 				{
 					ft_pa(&list_a, &list_b);
-					list_a->flag++;
+					//list_a->flag++;
 					ft_pa(&list_a, &list_b);
-					list_a->flag++;
+					//list_a->flag++;
 					par.size_b -= 2;
 				}
 			}
@@ -490,7 +742,7 @@ int main(int argc, char **argv)
 				else
 				{
 					ft_pa(&list_a, &list_b);
-					list_a->flag++;
+					//list_a->flag++;
 				}
 			}
 			if (list_a->next->order == par.next)
@@ -507,10 +759,14 @@ int main(int argc, char **argv)
 			//print_list(&list_a, &list_b);
 
 			if (if_sort_list(&list_a))
+			{
+				//print_list(&list_a, &list_b);
 				return (0);
+			}
 			par.c = 0;
 			while (par.flag >= 0)
 			{
+				//par.flag--;
 				while ((list_a->flag == par.flag) && list_a->order != 1)
 				{
 					ft_pb(&list_a, &list_b);
